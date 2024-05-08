7a32420c4410d54e5192e4a7f43129cd0d76ee88
kareem2002-k <kareemsamir166@gmail.com>
2024-05-08 20:46:02 +0300

    init in Syntax

---
+++ LexicalAnalyzer.java
@@ -0,0 +1,278 @@
+import java.util.*;
+import java.util.regex.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+
+public class LexicalAnalyzer {
+    public static class Token {
+        String type;
+        String value;
+        String scope;
+
+        Token() {
+            scope = "";
+        }
+    }
+
+    static Set<String> keywords = new HashSet<>(Arrays.asList(
+            "auto", "break", "case", "char", "const", "continue", "default",
+            "do", "double", "else", "enum", "extern", "float", "for", "goto",
+            "if", "int", "long", "register", "return", "short", "signed",
+            "sizeof", "static", "struct", "switch", "typedef", "union",
+            "unsigned", "void", "volatile", "while"
+    ));
+
+    static List<String> symbolTable = new ArrayList<>();
+    static Set<String> symbolSet = new HashSet<>();
+    static int scopeLevel = 0;
+
+    static List<Token> tokens = new ArrayList<>();
+
+    static boolean isDelimiter(char chr) {
+        return (chr == ' ' || chr == '+' || chr == '-' || chr == '*' ||
+                chr == '/' || chr == ',' || chr == ';' || chr == '%' ||
+                chr == '>' || chr == '<' || chr == '=' || chr == '(' ||
+                chr == ')' || chr == '[' || chr == ']' || chr == '{' ||
+                chr == '}' || chr == '!' || chr == '&' || chr == '|' || chr == '^' ||
+                chr == '~' || chr == '?' || chr == ':' || chr == '.' || chr == '\n');
+    }
+
+    static boolean isOperator(String str) {
+        Set<String> operators = new HashSet<>(Arrays.asList(
+                "+", "-", "*", "/", ">", "<", "=", "&", "|", "^", "~", "!", "%", "?", ":",
+                "==", "!=", "&&", "||", "++", "--", "+=", "-=", "*=", "/=", "%=", "&=", "|=", "^=",
+                ">>", "<<", "->", ".", "::", ".*"
+        ));
+        return operators.contains(str);
+    }
+
+    static boolean isOperator(char chr) {
+        return isOperator(String.valueOf(chr));
+    }
+
+    static boolean isValidIdentifier(String str) {
+        if (str.isEmpty() || Character.isDigit(str.charAt(0)) || isDelimiter(str.charAt(0))) {
+            return false;
+        }
+        for (char chr : str.toCharArray()) {
+            if (!Character.isLetterOrDigit(chr) && chr != '_') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static boolean isKeyword(String str) {
+        return keywords.contains(str);
+    }
+
+    static boolean isInteger(String str) {
+        Pattern intPattern = Pattern.compile("^[-+]?0$|^[-+]?[1-9][0-9]*$");
+        return intPattern.matcher(str).matches();
+    }
+
+    static boolean isOctal(String str) {
+        Pattern octalPattern = Pattern.compile("^0[0-7]+$");
+        return octalPattern.matcher(str).matches();
+    }
+
+    static boolean isBinary(String str) {
+        Pattern binaryPattern = Pattern.compile("^0b[01]+$");
+        return binaryPattern.matcher(str).matches();
+    }
+
+    static boolean isHexadecimal(String str) {
+        Pattern hexPattern = Pattern.compile("^0x[0-9a-fA-F]+$");
+        return hexPattern.matcher(str).matches();
+    }
+
+    static List<Token> lexicalAnalyzer(String input) {
+        String token = "";
+        boolean isString = false;
+        boolean isChar = false;
+        boolean isFloat = false;
+
+        for (int i = 0; i < input.length(); ++i) {
+            char chr = input.charAt(i);
+
+            if (isFloat) {
+                if (Character.isDigit(chr) || chr == '.' || (chr == 'e' || chr == 'E')) {
+                    token += chr;
+                    if ((chr == 'e' || chr == 'E') && (input.charAt(i + 1) == '-' || input.charAt(i + 1) == '+')) {
+                        token += input.charAt(i + 1);
+                        ++i;
+                    }
+                    continue;
+                } else {
+                    Token t = new Token();
+                    t.type = "Float";
+                    t.value = token;
+                    tokens.add(t);
+                    token = "";
+                    isFloat = false;
+                }
+            }
+
+            if (chr == '\"') {
+                if (!isString) {
+                    isString = true;
+                } else {
+                    Token t = new Token();
+                    t.type = "String Literal";
+                    t.value = token;
+                    tokens.add(t);
+                    token = "";
+                    isString = false;
+                }
+            } else if (chr == '\'') {
+                if (!isChar) {
+                    isChar = true;
+                } else {
+                    Token t = new Token();
+                    t.type = "Character Literal";
+                    t.value = token;
+                    tokens.add(t);
+                    token = "";
+                    isChar = false;
+                }
+            } else if (isString || isChar) {
+                token += chr;
+            } else if (chr == '#') {
+                while (i < input.length() && input.charAt(i) != '\n') {
+                    ++i;
+                }
+            } else if (chr == '/' && i + 1 < input.length() && (input.charAt(i + 1) == '/' || input.charAt(i + 1) == '*')) {
+                if (input.charAt(i + 1) == '/') {
+                    while (i < input.length() && input.charAt(i) != '\n') {
+                        ++i;
+                    }
+                } else {
+                    while (i + 1 < input.length() && !(input.charAt(i) == '*' && input.charAt(i + 1) == '/')) {
+                        ++i;
+                    }
+                    ++i;
+                }
+            } else if (isDelimiter(chr)) {
+                if (!token.isEmpty() && !Pattern.matches("^\\s*$", token)) {
+                    Token t = new Token();
+                    if (isKeyword(token)) {
+                        t.type = "Keyword " + token;
+                        t.value = token;
+                    } else if (isInteger(token)) {
+                        t.type = "Integer";
+                        t.value = token;
+                    } else if (isFloat) {
+                        t.type = "Float";
+                        t.value = token;
+                    } else if (isValidIdentifier(token)) {
+                        t.type = "Identifier " + token;
+                        t.value = "index " + symbolTable.size();
+                        t.scope += " " + scopeLevel;
+
+                        if (chr == '(') {
+                            t.type = "Identifier Function " + token;
+                            token += "()";
+                        }
+
+                        if (!symbolSet.contains(token)) {
+                            symbolTable.add(token);
+                            symbolSet.add(token);
+                        }
+                    } else if (isOperator(token)) {
+                        t.type = "Operator " + token;
+                        t.value = token;
+                    } else if (isHexadecimal(token)) {
+                        t.type = "Integer Hexadecimal";
+                        t.value = token;
+                    } else if (isOctal(token)) {
+                        t.type = "Integer Octal";
+                        t.value = token;
+                    } else if (isBinary(token)) {
+                        t.type = "Integer Binary";
+                        t.value = token;
+                    } else {
+                        t.type = "Unidentified";
+                        t.value = token;
+                    }
+                    tokens.add(t);
+                    token = "";
+                }
+
+                int j = i + 1;
+                while (j < input.length() && Character.isWhitespace(input.charAt(j))) {
+                    ++j;
+                }
+                if (j < input.length() && isOperator(String.valueOf(chr) + String.valueOf(input.charAt(j)))) {
+                    Token t = new Token();
+                    t.type = "Operator " + String.valueOf(chr) + String.valueOf(input.charAt(j));
+                    t.value = String.valueOf(chr) + String.valueOf(input.charAt(j));
+                    tokens.add(t);
+                    i = j;
+                } else if (isOperator(chr)) {
+                    Token t = new Token();
+                    t.type = "Operator " + String.valueOf(chr);
+                    t.value = String.valueOf(chr);
+                    tokens.add(t);
+                } else if (chr != ' ' && chr != '\n') {
+                    Token t = new Token();
+                    t.type = "Delimiter " + String.valueOf(chr);
+                    t.value = String.valueOf(chr);
+                    tokens.add(t);
+                }
+            } else if (Character.isDigit(chr)) {
+                token += chr;
+                if (i + 1 < input.length() && (input.charAt(i + 1) == '.' || input.charAt(i + 1) == 'e' || input.charAt(i + 1) == 'E'
+                        || input.charAt(i + 1) == 'e' && input.charAt(i+2) == '-' || input.charAt(i + 1) == 'E' && input.charAt(i+2) == '-')) {
+                    isFloat = true;
+                }
+            } else {
+                token += chr;
+            }
+            if (chr == '{') {
+                ++scopeLevel;
+            } else if (chr == '}') {
+                --scopeLevel;
+            }
+        }
+
+        return tokens;
+    }
+
+    static void printTokens(List<Token> tokens) {
+        for (Token token : tokens) {
+            System.out.println("Token: " + token.type + ", Value: " + token.value);
+            if (!token.scope.isEmpty()) {
+                System.out.println(", Scopes: " + token.scope);
+            } else {
+                System.out.println();
+            }
+        }
+    }
+
+    static void printSymbolTable(List<Token> tokens) {
+        System.out.println("----------------Symbol Table---------------");
+        for (int i = 0; i < symbolTable.size(); ++i) {
+            System.out.println("Identifier: " + symbolTable.get(i) + ", Index: " + i);
+        }
+    }
+
+    public static void main(String[] args) {
+        // String filePath = "C:\\Users\\Ahmed\\Desktop\\ya rab\\input.c"; // replace with your file path
+        // StringBuilder content = new StringBuilder();
+        // try {
+        //     Files.lines(Paths.get(filePath)).forEach(line -> content.append(line).append("\n"));
+        // } catch (IOException e) {
+        //     System.err.println("Error reading file " + filePath);
+        //     e.printStackTrace();
+        //     return;
+        // }
+
+        String input = "int x ";
+        List<Token> tokens = lexicalAnalyzer(input);
+        SyntaxAnalyzer syntaxAnalyzer = new SyntaxAnalyzer(tokens);
+        syntaxAnalyzer.parse();
+        printTokens(tokens);
+        printSymbolTable(tokens);
+    }
+}
--- SyntaxAnalyzer.java
+++ SyntaxAnalyzer.java
@@ -1,5 +1,4 @@
-
-
+import java.util.List;
 
 /**
  TEST case1 : printf("Hello World");
@@ -10,142 +9,314 @@
                 int x = 10;
               }
 
+
+
+program : declaration_list 
+
+declaration_list → declaration declaration_list | ε
+
+
+declaration : var_declaration
+            | fun_declaration
+            | struct_declaration
+            | enum_declaration
+            | typedef_declaration
+
+var_declaration : type_specifier ID ';'
+                | type_specifier ID '[' expression ']' ';'
+                | type_specifier pointer ID ';'
+                | type_specifier pointer ID '[' expression ']' ';'
+
+pointer : '*' | pointer '*'
+
+type_specifier : int
+               | void
+               | char
+               | float
+               | double
+               | struct ID
+               | enum ID
+               | typedef_name
+
+typedef_declaration : typedef type_specifier ID ';'
+                    | typedef type_specifier ID '[' expression ']' ';'
+                    | typedef type_specifier pointer ID ';'
+                    | typedef type_specifier pointer ID '[' expression ']' ';'
+
+fun_declaration : type_specifier ID '(' params ')' compound_stmt
+
+params : param_list
+       | void
+
+param_list : param_list ',' param
+           | param
+
+param : type_specifier ID
+      | type_specifier ID '[' ']'
+
+struct_declaration : struct ID '{' struct_var_declaration_list '}' ';'
+
+struct_var_declaration_list : struct_var_declaration_list var_declaration
+                            | var_declaration
+
+enum_declaration : enum ID '{' enum_var_list '}' ';'
+
+enum_var_list : enum_var_list ',' ID
+              | ID
+
+compound_stmt : '{'statement_list '}'
+
+local_declarations : local_declarations var_declaration| local_declarations enum_declaration |
+                   | empty
+
+statement_list : statement_list statement
+               | empty
+
+statement : local_declarations 
+          | expression_stmt
+          | compound_stmt
+          | selection_stmt
+          | iteration_stmt
+          | return_stmt
+
+expression_stmt : expression ';'
+                | ';'
+
+selection_stmt : if '(' expression ')' statement
+               | if '(' expression ')' statement else statement
+
+iteration_stmt : while '(' expression ')' statement
+               | for '(' expression_stmt expression ';' expression ')' statement
+               | do statement while '(' expression ')' ';'
+
+return_stmt : return ';'
+            | return expression ';'
+
+expression : assignment_expression
+
+assignment_expression : conditional_expression
+                       | unary_expression assignment_operator assignment_expression
+
+assignment_operator : '='
+
+conditional_expression : logical_or_expression
+                        | logical_or_expression '?' expression ':' conditional_expression
+
+logical_or_expression : logical_and_expression
+                      | logical_or_expression '||' logical_and_expression
+
+logical_and_expression : inclusive_or_expression
+                       | logical_and_expression '&&' inclusive_or_expression
+
+inclusive_or_expression : exclusive_or_expression
+                         | inclusive_or_expression '|' exclusive_or_expression
+
+exclusive_or_expression : and_expression
+                         | exclusive_or_expression '^' and_expression
+
+and_expression : equality_expression
+               | and_expression '&' equality_expression
+
+equality_expression : relational_expression
+                    | equality_expression '==' relational_expression
+                    | equality_expression '!=' relational_expression
+
+relational_expression : shift_expression
+                       | relational_expression '<' shift_expression
+                       | relational_expression '>' shift_expression
+                       | relational_expression '<=' shift_expression
+                       | relational_expression '>=' shift_expression
+
+shift_expression : additive_expression
+                  | shift_expression '<<' additive_expression
+                  | shift_expression '>>' additive_expression
+
+additive_expression : multiplicative_expression
+                     | additive_expression '+' multiplicative_expression
+                     | additive_expression '-' multiplicative_expression
+
+multiplicative_expression : cast_expression
+                           | multiplicative_expression '*' cast_expression
+                           | multiplicative_expression '/' cast_expression
+                           | multiplicative_expression '%' cast_expression
+
+cast_expression : unary_expression
+                | '(' type_name ')' cast_expression
+
+unary_expression : postfix_expression
+                 | '++' unary_expression
+                 | '--' unary_expression
+                 | unary_operator cast_expression
+                 | sizeof unary_expression
+                 | sizeof '(' type_name ')'
+
+unary_operator : '&'
+               | '*'
+               | '+'
+               | '-'
+               | '~'
+               | '!'
+
+postfix_expression : primary_expression
+                    | postfix_expression '[' expression ']'
+                    | postfix_expression '(' argument_expression_list ')'
+                    | postfix_expression '.' ID
+                    | postfix_expression '->' ID
+                    | postfix_expression '++'
+                    | postfix_expression '--'
+                    | ID '(' argument_expression_list ')'
+
+primary_expression : ID
+                    | constant
+                    | string
+                    | '(' expression ')'
+
+constant : NUM
+
+string : STRING_LITERAL
+
+argument_expression_list : assignment_expression
+                         | argument_expression_list ',' assignment_expression
+
+
   
 
  **/
 
-
-
-
-
-
-// Grammar RULES for the C- language
-// program -> declaration_list |initial_statement_list declaration_list
-
-// initial_statement_list -> initial_statement
-//                         | initial_statement_list initial_statement
-
-// initial_statement -> function_call_statement
-//                     | other_initial_statement
-
-// function_call_statement -> function_call ;
-//                          | function_call_statement function_call
-
-// other_initial_statement -> declaration
-//                          | struct_declaration
-
-// declaration_list -> declaration
-//                   | declaration_list declaration | Ɛ
-
-// declaration -> variable_declaration
-//              | function_declaration
-
-// variable_declaration -> type_specifier ID ;
-//                       | type_specifier ID [ NUM ] ;
-
-// type_specifier -> int
-//                 | float
-//                 | char
-//                 | double
-//                 | void
-
-// function_declaration -> type_specifier ID ( parameters ) compound_statement
-
-// parameters -> parameter_list
-//             | void
-
-// parameter_list -> parameter
-//                 | parameter_list , parameter
-
-// parameter -> type_specifier ID
-//            | type_specifier ID []
-
-// compound_statement -> { statement_list }
-
-// statement_list -> statement
-//                  | statement_list statement
-
-// statement ->  if_statement | while_statement | do_while_statement | for_statement | return_statement | switch_statement | compound_statement | declaration | assignment_statement | break_statement | continue_statement | switch_cases
-
-// expression_statement -> expression ;
-//                       | ;
-
-// expression -> ID = expression
-//             | simple_expression
-
-// simple_expression -> additive_expression
-//                    | simple_expression RELOP additive_expression
-
-// additive_expression -> multiplicative_expression
-//                       | additive_expression ADDOP multiplicative_expression
-
-// multiplicative_expression -> primary_expression
-//                             | multiplicative_expression MULOP primary_expression
-
-// primary_expression -> ID
-//                      | NUM
-//                      | ( expression )
-//                      | function_call
-
-// function_call -> ID ( argument_list )
-
-// argument_list -> expression
-//                 | argument_list , expression
-
-// selection_statement -> if ( expression ) statement
-//                      | if ( expression ) statement else statement
-
-// iteration_statement -> while ( expression ) statement
-//                      | for ( expression_statement ; expression_statement ; expression ) statement
-
-// jump_statement -> return ;
-//                 | return expression ;
-
-// class_declaration -> class ID { member_declaration_list }
-
-// member_declaration_list -> member_declaration
-//                          | member_declaration_list member_declaration
-
-// member_declaration -> type_specifier ID ;
-//                     | type_specifier ID ( parameters ) compound_statement ;
-
-// struct_declaration -> struct ID { struct_member_list }
-
-// struct_member_list -> struct_member
-//                      | struct_member_list struct_member
-
-// struct_member -> type_specifier ID ;
-//                | type_specifier ID [ NUM ] ;
-
-
-
-
 public class SyntaxAnalyzer {
-    private int currentTokenIndex;
-    private Token[] tokens;
+    public String TYPE_SPECIFIER = "Keyword (int|float|char|double|short|long|signed|unsigned|void|String|bool)";
+    // public String KEYWORD = "Keyword *";
+    public String IDENTIFIER = "Identifier .*"; 
 
-    public SyntaxAnalyzer(Token[] tokens) {
+    private int currentTokenIndex;
+    private List<LexicalAnalyzer.Token> tokens;
+
+    public SyntaxAnalyzer(List<LexicalAnalyzer.Token> tokens) {
         this.tokens = tokens;
         this.currentTokenIndex = 0;
 
     }
 
 
-    private boolean match(Token expected) {
-        if (currentTokenIndex < tokens.length && tokens[currentTokenIndex].equals(expected)) {
+    private boolean match(String expected) {
+       
+     
+        if (currentTokenIndex < tokens.size() && tokens.get(currentTokenIndex).type.matches(expected)) {
+            System.out.println("Current token index: " + currentTokenIndex);
+
+            System.out.println("Matched: " + tokens.get(currentTokenIndex).type + " " + tokens.get(currentTokenIndex).value);
             return true;
+        } else {
+            return false;
+            
         }
-        return false;
     }
+
+
 
 
     public void advance() {
-        currentTokenIndex++;
+        if (currentTokenIndex >= tokens.size()-1) {
+            System.out.println("End of tokens"+ currentTokenIndex);
+            return;
+            
+        }
+        this.currentTokenIndex++;
+
+        System.out.println("Advancing to: " + tokens.get(currentTokenIndex).type + " " + tokens.get(currentTokenIndex).value);
+    }
+
+    public void parse () {
+        program();
     }
 
 
+      private void program() {
+        declaration_list();        
+        
+
+    }
+
+    private void declaration_list() {
+
+        if (match(TYPE_SPECIFIER)) {
+            
+            declaration();
+            declaration_list();
+
+        } else {
+            return;
+        }
+     
+    }
+
+    private void declaration() {
+
+        // while (match(KEYWORD)) {
+        //     advance();       
+        // }
+
+        if (match(TYPE_SPECIFIER)) {
+            advance();
+          if (match(IDENTIFIER)) {
+                advance();
+              if (match("Delimiter \\[")) {
+                    advance();
+                    // array declaration       
+                } else if (match("Operator \\*")) {
+
+                    while (match("Operator \\*")) {
+                        advance();                       
+                    }
+
+                    // pointer declaration
+
+                } else if (match("Delimiter \\(")) {
+                    advance();
+                    // function declaration
+                }else {
+                    advance();
+
+                    var_declaration();
+
+                    //var_declaration();
+                  
+                }
+            }
+
+        }else {
+            System.out.println("Invalid Declaration");
+            return;
+        }
+  
+    }
+
+    private void var_declaration() {
+
+        if (match("Operator =")) {
+            advance();
+           // expression();
+        
+           if ( match("Delimiter ;")) {
+            advance();
+            System.out.println("Variable declaration");
+            
+           }else {
+               System.out.println("Syntax Error missing in assignment  ;");
+           }
+            
+        }else if (match("Delimiter ;")) {
+            System.out.println("Variable declaration");
+            return;
+          
+        }else{
+            System.out.println("Syntax Error missing but without assignment ;");
+        }
 
 
-    
+
+
+    }
+ 
 
 }
